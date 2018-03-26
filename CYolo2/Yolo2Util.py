3a61ede0065886f8ad7d2610e0669c639453d041
blade <CJHdream@outlook.com>
2018-03-26 20:57:47 +0800

    change [x, y] format to [y, x]

--- CYolo2/Yolo2Util.py
+++ CYolo2/Yolo2Util.py
@@ -9,11 +9,11 @@ class Yolo2Util:
     def __init__(self, image_shape, number_class, cluster_number, scalar, prior_shape):
         """
         
-        :param image_shape:输入图像的形状[width, height]
+        :param image_shape:输入图像的形状[height, width]
         :param number_class:目标检测类别数+1
         :param cluster_number:先验框数量
-        :param scalar:yolo2特征提取尺度，论文采用的32x32
-        :param prior_shape:聚类获得的先验框的宽高[[w, h], [w, h], ...]
+        :param scalar:yolo2特征提取尺度[height, width]，论文采用的32x32
+        :param prior_shape:聚类获得的先验框的宽高[[h, w], [h, w], ...]
         """
         self.ImageShape = image_shape
         self.NumberClass = number_class
@@ -40,51 +40,51 @@ class Yolo2Util:
         self.ClassLabelData = classes_data
         pass
 
-    def _abandon_or_not(self, x_y):
+    def _abandon_or_not(self, y_x):
         """
         先验box超出图像的部分，不进行采用，原因：label与数据变得无关，反而会使回归混乱
         :param x_y:单个样本真实的x, y 坐标:[x, y]
         :return:bool_list:对于一系列prior box建议采用的判断：true采用， false不采用
         """
-        [x_conformity, y_conformity] = np.array(x_y) - np.array(x_y) % np.float64(self.Scalar)
-        [min_x_in_image, min_y_in_image] = np.min(
+        [y_conformity, x_conformity] = np.array(y_x) - np.array(y_x) % np.float64(self.Scalar)
+        [min_y_in_image, min_x_in_image] = np.min(
             np.array(
-                [[x_conformity, self.ImageShape[0] - x_conformity],
-                 [y_conformity, self.ImageShape[1] - y_conformity]]),
+                [[y_conformity, self.ImageShape[0] - y_conformity],
+                 [x_conformity, self.ImageShape[1] - x_conformity]]),
             axis=1)
-        flag = np.ceil(np.array(self.PriorShape) / 2.0) < np.array([min_x_in_image, min_y_in_image])
+        flag = np.ceil(np.array(self.PriorShape) / 2.0) < np.array([min_y_in_image, min_x_in_image])
         flag = flag[:, 0] & flag[:, 1]
         return list(flag)
         pass
 
-    def _generate_coordinate_x_y(self,  x_y):
+    def _generate_coordinate_y_x(self,  y_x):
         """
 
-        :param x_y:真值box的中心坐标，shape=[2], [x, y] list
+        :param x_y:真值box的中心坐标，shape=[2], [y, x] list
         :return:返回X, Y, X_Shift, Y_Shift: 上采样之后位置[X, Y]为coordinate输出位置， [X_shift,Y_Shift]:论文中提到的偏移量
         """
-        [X, Y] = np.array(np.array(x_y) / self.Scalar, np.int64)
-        shift = np.array(x_y) % self.Scalar
-        [X_Shift, Y_Shift] = shift / np.float64(self.Scalar)
-        return X, Y, X_Shift, Y_Shift
+        [Y, X] = np.array(np.array(x_y) / self.Scalar, np.int64)
+        shift = np.array(y_x) % self.Scalar
+        [Y_Shift, X_Shift] = shift / np.float64(self.Scalar)
+        return Y, X, Y_Shift, X_Shift
         pass
 
-    def _generate_coordinate_w_h(self, prior_w_h, w_h):
+    def _generate_coordinate_h_w(self, prior_h_w, h_w):
         """
 
-        :param prior_w_h:先验box的宽与高，shape=[2], [width, height] list
-        :param w_h:真值box的宽与高，shape=[2], [width, height] list
-        :return:输出偏移量, shape=[2], [width_shift, height_shift], list
+        :param prior_h_w:先验box的宽与高，shape=[2], [height, width] list
+        :param h_w:真值box的宽与高，shape=[2], [height, width] list
+        :return:输出偏移量, shape=[2], [height_shift, width_shift], list
         """
-        w_h_shift = np.log(np.array(prior_w_h, np.float64) / np.array(w_h, np.float64))
-        return list(w_h_shift)
+        h_w_shift = np.log(np.array(prior_h_w, np.float64) / np.array(h_w, np.float64))
+        return list(h_w_shift)
         pass
 
     def _generate_coordinate_confidence(self, pre_coordinate, coordinate):
         """
         
-        :param pre_coordinate:预测的坐标四元组， shape=[4], [pre_x_shift, pre_y_shift, pre_w_shift, pre_h_shift] 
-        :param coordinate: 真实的坐标四元组， shape=[4], [x_shift, y_shift, w_shift, h_shift]
+        :param pre_coordinate:预测的坐标四元组， shape=[4], [pre_y_shift, pre_x_shift, pre_h_shift, pre_w_shift] 
+        :param coordinate: 真实的坐标四元组， shape=[4], [y_shift, x_shift, y_shift, w_shift]
         :return: 
         """
         p = [self.Scalar * pre_coordinate[1],
@@ -107,31 +107,31 @@ class Yolo2Util:
         通过参数列表生成label tensor
         :param argv_list: 参数列表，{'0': 
         [
-        {indexes': [indexes_x, indexes_y], 
-        'x_y_shift': [x_shift, y_shift], 
-        'w_h_shift': [[w_shift, h_shift], ...],
+        {indexes': [indexes_y, indexes_x], 
+        'y_x_shift': [y_shift, x_shift], 
+        'h_w_shift': [[h_shift, w_shift], ...],
         'pre_truth_IoU': [ , , ...],
         'class': 
         }, ...},
          dict 有多组先验w 与 h 需要有多组w_h_shift
         :return: 返回label tensor：[H, W, C]
         """
-        LabelCoordinateArray = np.zeros([self.Check[1], self.Check[0], self.ClusterNumber * 5], np.float64)
-        LabelClassesArray = np.zeros([self.Check[1], self.Check[0], self.ClusterNumber * self.NumberClass], np.float64)
-        LabelMaskObjArray = np.zeros([self.Check[1], self.Check[0]], np.float64)
-        LabelMaskNoobjArray = np.ones([self.Check[1], self.Check[0]], np.float64)
+        LabelCoordinateArray = np.zeros([self.Check[0], self.Check[1], self.ClusterNumber * 5], np.float64)
+        LabelClassesArray = np.zeros([self.Check[0], self.Check[1], self.ClusterNumber * self.NumberClass], np.float64)
+        LabelMaskObjArray = np.zeros([self.Check[0], self.Check[1]], np.float64)
+        LabelMaskNoobjArray = np.ones([self.Check[0], self.Check[1]], np.float64)
         for i in list(argv_list.keys()):
             cla = [0.0] * self.NumberClass
             cla[argv_list[i]['class']] = 1.0
             y = argv_list[i]['indexes'][1]
             x = argv_list[i]['indexes'][0]
-            x_shift = argv_list[i]['x_y_shift'][0]
-            y_shift = argv_list[i]['x_y_shift'][1]
+            y_shift = argv_list[i]['y_x_shift'][0]
+            x_shift = argv_list[i]['y_x_shift'][1]
             for j in range(0, self.ClusterNumber):
-                w_shift = argv_list[i]['w_h_shift'][j][0]
-                h_shift = argv_list[i]['w_h_shift'][j][1]
+                h_shift = argv_list[i]['w_h_shift'][j][0]
+                w_shift = argv_list[i]['w_h_shift'][j][1]
                 iou = argv_list[i]['pre_truth_IoU'][j]
-                LabelCoordinateArray[y, x, j * 5: (j + 1) * 5] = [x_shift, y_shift, w_shift, h_shift, iou]
+                LabelCoordinateArray[y, x, j * 5: (j + 1) * 5] = [y_shift, x_shift, h_shift, w_shift, iou]
             LabelClassesArray[y, x] = cla * self.ClusterNumber
             LabelMaskObjArray[y, x] = 1.0
             LabelMaskNoobjArray[y, x] = 0.0
@@ -163,11 +163,11 @@ class Yolo2Util:
     def label_data_to_label(self, coordinate_data, classes_data, pre_coordinate):
         """
         预测了单张图像，可含有多个obj，建议不要有多个obj都在同一cell之中
-        :param coordinate_data:真值坐标数据， shape=[obj_num, 4], [[x, y, w, h], ...], list
+        :param coordinate_data:真值坐标数据， shape=[obj_num, 4], [[y, x, h, w], ...], list
         :param classes_data:真值类别数据，shape=[obj_num], [[0], [1], ...],list， 0代表背景，
         顺序需要与coordinate_data相对应
         :param pre_coordinate:obj位置预测生成的坐标数据，shape=[obj_num, cluster_num, 4]
-        [[[pre_x_shift, pre_y_shift, pre_w_shift, pre_h_shift], ...], ...]
+        [[[pre_y_shift, pre_x_shift, pre_h_shift, pre_w_shift], ...], ...]
         :return:
         """
         if type(coordinate_data).__name__ == 'ndarray':
@@ -179,24 +179,24 @@ class Yolo2Util:
         assert self._check_info(coordinate_data, classes_data, pre_coordinate) is True
         argv_dict = dict()
         for i in range(0, len(coordinate_data)):
-            indexes_x, indexes_y, x_shift, y_shift = \
-                self._generate_coordinate_x_y([coordinate_data[i][0], coordinate_data[i][1]])
+            indexes_y, indexes_x, y_shift, x_shift = \
+                self._generate_coordinate_y_x([coordinate_data[i][0], coordinate_data[i][1]])
             argv_dict[i] = dict()
-            argv_dict[i]['indexes'] = [indexes_x, indexes_y]
-            argv_dict[i]['x_y_shift'] = [x_shift, y_shift]
+            argv_dict[i]['indexes'] = [indexes_y, indexes_x]
+            argv_dict[i]['y_x_shift'] = [y_shift, x_shift]
             w_h_shift = list()
             pre_truth_IoU = list()
             for j in range(0, len(self.PriorShape)):
-                w_shift, h_shift = \
-                    self._generate_coordinate_w_h(
+                h_shift, w_shift = \
+                    self._generate_coordinate_h_w(
                         self.PriorShape[j], [coordinate_data[j][2], coordinate_data[j][3]])
                 t_confidence = \
                     self._generate_coordinate_confidence(
                         pre_coordinate[i][j],
-                        [x_shift, y_shift, w_shift, h_shift])
-                w_h_shift.append([w_shift, h_shift])
+                        [y_shift, x_shift, h_shift, w_shift])
+                w_h_shift.append([h_shift, w_shift])
                 pre_truth_IoU.append(t_confidence)
-            argv_dict[i]['w_h_shift'] = w_h_shift
+            argv_dict[i]['h_w_shift'] = w_h_shift
             argv_dict[i]['pre_truth_IoU'] = pre_truth_IoU
             argv_dict[i]['class'] = classes_data
         Label = self._argv_to_label(argv_list=argv_dict)
