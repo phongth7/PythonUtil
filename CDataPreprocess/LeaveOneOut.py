a38c037d061f7c6fbab9bd31ab2af82c1b923ddd
sins <CJHdream@outlook.com>
2018-04-06 23:52:52 +0800

    not complete

--- CDataPreprocess/LeaveOneOut.py
+++ CDataPreprocess/LeaveOneOut.py
@@ -4,14 +4,25 @@ import random
 import json
 import sys
 
+import CFunction.Sample as CFS
+
 class LeaveOneOut:
     def __init__(self):
         self.Type = str()
         self.File = list()
         pass
 
-    def csv_configure(self, file_list_full_path, rate, unique_identification, out_put_file_path, type='speed'):
+    def csv_features_seperated(
+            self,
+            file_list_full_path,
+            rate,
+            unique_identification,
+            out_put_file_path,
+            type='speed',
+            ignore_range=[]):
         """
+        针对样本的特征存储在多个文件中，但是这多个文件有对应的唯一标志，比如a特征存在a文件，b特征存在b文件，但是对于K样本，
+        在两个文件中都有对应的第m列标志k在相同，同时对于其他样本又不同
         csv多文件留一数据预处理，要求file_list_full_path中的标记拥有同等数据量
         :param file_list_full_path:list 多个文件的列表，考虑到可能多种标记
         :param out_put_file_path:输出文件，依靠该文件，规范化读取数据
@@ -27,14 +38,19 @@ class LeaveOneOut:
             part_num = round(1 / rate)
             for i in file_list_full_path:
                 reader = csv.reader(open(i, 'r'))
+                # 取标识列信息，存到字典key_info中，该文件的绝对路径作为键值
                 key_info[i] = [row[unique_identification] for row in reader]
             assert self._check_same_length(key_info) is True, 'not all csv file has the same number of data'
+            # part_num次留一法，每个留一法验证集互斥
             for i in range(0, part_num):
-                index = self._random_select(key_info['0'], rate=rate)
+                index = CFS.sample_except(
+                    key_info[unique_identification[0]],
+                    sample_amount=int(rate * len(key_info[unique_identification[0]])),
+                    except_list=ignore_range)
                 info[i] = dict()
                 info[i][file_list_full_path] = index
                 for j in file_list_full_path[1: ]:
-                    l = [key_info[j].index(key_info[file_list_full_path[1]][k]) for k in index]
+                    l = [key_info[j].index(key_info[file_list_full_path[j]][k]) for k in index]
                     info[i][j] = l
                     pass
         elif type == 'memory':
@@ -54,6 +70,13 @@ class LeaveOneOut:
 
     def csv_reader(self):
 
+        pass
+
+    def _remove_ignore_element(self):
+        """
+        对csv.reader读取出来的
+        :return:
+        """
         pass
 
     def _check_same_length(self, dict_input):
@@ -80,11 +103,13 @@ class LeaveOneOut:
 
     def _random_select(self, list_input, rate):
         """
-        以rate概率随机选择列表中的成员，返回选择到的成员的索引
+        以rate概率随机选择列表中的成员，删除选中的成员，返回选择到的成员的索引
         :param list_input:输入的list
         :param rate:概率。
         :return:
         """
         x = range(0, len(list))
+        for i in x:
+            del list_input[i]
         return random.sample(x, int(len(x) * rate))
         pass
