ecf8e8079c9498bc80e4a8ddf3e186d08fc0dd4b
blade <CJHdream@outlook.com>
2018-04-07 21:21:49 +0800

    change test to non-integer part

--- test/CDataPreprocess/LeaveOneOut.py
+++ test/CDataPreprocess/LeaveOneOut.py
@@ -5,7 +5,7 @@ def test_csv_features_seperated():
     a = LOO.LeaveOneOut()
     a.csv_features_seperated(
         ['./data/test_LeaveOneOut.csv', './data/test_LeaveOneOut2.csv'],
-        rate=0.1,
+        rate=0.3,
         unique_identification=0,
         out_put_file_path='./data/test_csv_features_seperated.json',
         ignore_indexes=[0])
